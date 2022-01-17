d14c4c6b03b255c5f42e9f94d7a3183ff9d0343a
Ernesto Hernandez <gastronomo76@gmail.com>
2022-01-16 22:20:16 -0500

    ACTUALIZE EL ULTIMO CAMBIO 1 code smell

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.devopsusach2020;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devopsusach2020.model.Pais;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+    Pais p = new Pais();
+    assertNotNull(p);
 	}
 
 }
